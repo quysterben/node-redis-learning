167d4f2b1b7669a82494d31df93a82ae3db7f346
Hoang Sy Quy <kiaisoft.quy.hoang@gmail.com>
2024-05-30 09:17:52 +0700

    initial/add-redis

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import path from 'path'
-
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import multer from 'multer'
-
 import dotenv from 'dotenv'
 import helmet from 'helmet'
-
 import cookieParser from 'cookie-parser'
+
+import serverRoute from './routes/router'
 
 import passport from 'passport'
 // import { applyPassportStrategy } from './store/passport'
-
-import serverRoute from './routes/router'
 
 dotenv.config()
 
@@ -22,12 +18,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(helmet())
 app.use(cors())
 
-// Passport
 // applyPassportStrategy(passport)
 
 app.use(cookieParser())
 
-// Multer
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'public/images')
@@ -44,9 +38,7 @@ const fileFilter = (req: Request, file: any, cb: any) => {
   }
 }
 app.use(multer({ storage: fileStorage, fileFilter }).array('image', 10))
-app.use('/res/images', express.static(path.join(__dirname, 'res', 'images')))
 
-// Routes
 app.get('/', (req: Request, res: Response) => {
   res.json({ messenge: 'Hello xin chao' })
 })
